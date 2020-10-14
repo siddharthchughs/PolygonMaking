98a27ca9f0990d59573f73070a1b314245412bc2
siddharth <siddharthchughs@gmail.com>
2020-10-14 21:58:13 +0530

    new commit

--- mypolygonmap/src/main/java/com/medical/mypolygonmap/Activities/UtilityHelper/FileHelper.kt
+++ mypolygonmap/src/main/java/com/medical/mypolygonmap/Activities/UtilityHelper/FileHelper.kt
@@ -1,6 +1,7 @@
 package com.medical.mypolygonmap.Activities.UtilityHelper
 
 import android.content.Context
+import android.icu.text.SimpleDateFormat
 import android.widget.Toast
 
 class FileHelper {
@@ -15,6 +16,29 @@ class FileHelper {
         }
 
 
+//   var dateFormat = "2020-09-01T08:00:00.000Z"
+//        var formatter = SimpleDateFormat("yyyy MM dd", Locale.ENGLISH)
+//        var mDate = formatter.parse(dateFormat).toString()
+//        Log.e("the", "date is" + dateFormat)
+
+//        val sdf = SimpleDateFormat("yyyy-MM-dd")
+//        //     val date: Date = sdf.parse(dateFormat)
+//        ///  sdf.setTimeZone(TimeZone.getTimeZone("IST"))
+//        // System.out.println(sdf.format(date))
+//        //  Log.e("the", "date is" + dateFormat)
+//
+//        //  val sdf = SimpleDateFormat("dd-MM-yyyy")
+//        //    val timeonly = SimpleDateFormat("hh:mm")
+//        //  val dateString = sdf.format(dateFormat)
+//        //  val timeString = timeonly.format(dateFormat)
+//        //show present date
+//        //show present date
+//
+//        //
+//        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+//        val outputFormat = SimpleDateFormat("dd-MM-yyyy")
+//        val date = inputFormat.parse("2018-04-10T04:00:00.000Z")
+//        val formattedDate = outputFormat.format(date)
 
 
     }
